53359ed9e83abc17c6dbb4041dee20c43ded7985
Hikaru2070 <134835834+Hikaru2070@users.noreply.github.com>
2025-08-28 17:06:26 +0900

    change asset url
    アセットのURLをHTTPからHTTPSに変更

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://launcher-assets.mc.gdev.f5.si/ModPacks/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://launcher-assets.mc.gdev.f5.si/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
